89a68dd1662caa69bdf7774a0fa0b7ff1ca66eac
br4dyb <154681345+br4dyb@users.noreply.github.com>
2025-05-05 02:04:14 -0500

    ~

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,9 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, InteractionContextType } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
-		.setDescription('Provides information about the server.'),
+		.setDescription('Provides information about the server.')
+		.setContexts(InteractionContextType.Guild),
 	async execute(interaction) {
 		// interaction.guild is the object representing the Guild in which the command was run
 		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,9 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, InteractionContextType } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
-		.setDescription('Provides information about the user.'),
+		.setDescription('Provides information about the user.')
+		.setContexts(InteractionContextType.Guild),
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
