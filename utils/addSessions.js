c85181741162356ab6f0180a5b276328f7e0e894
br4dyb <154681345+br4dyb@users.noreply.github.com>
2025-05-06 20:33:28 -0500

    ~

--- utils/addSessions.js
+++ utils/addSessions.js
@@ -5,12 +5,19 @@ function generateId() {
     return 'e_' + Date.now().toString(36) + Math.random().toString(36).slice(2, 6);
 }
 
+function generateTimestamp() {
+    let eventDate = new Date();
+    let eventDateTimestamp = Math.floor(eventDate.getTime() / 1000);
+    return eventDateTimestamp;
+}
+
 // Generate Event Function:
 async function addSession() {
     console.log('[i] Adding session...');
 
     await sessionManager.saveSession(generateId(), {
-        date: new Date().toUTCString(),
+        type: 'Training',
+        date: generateTimestamp(),
         host: null,
         trainers: {},
         location: 'https://roblox.com',
