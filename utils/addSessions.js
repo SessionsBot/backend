ecae43bc50a2795b28f86a5c2c0d3cea89be6348
br4dyb <154681345+br4dyb@users.noreply.github.com>
2025-05-06 21:58:07 -0500

    ~

--- utils/addSessions.js
+++ utils/addSessions.js
@@ -5,11 +5,65 @@ function generateId() {
     return 'e_' + Date.now().toString(36) + Math.random().toString(36).slice(2, 6);
 }
 
-function generateTimestamp() {
+function generateTimestamp(hourOfDay) {
     let eventDate = new Date();
+    eventDate.setHours(hourOfDay || 7);
+    eventDate.setMinutes(30)
     let eventDateTimestamp = Math.floor(eventDate.getTime() / 1000);
     return eventDateTimestamp;
 }
+
+// Generate Todays Training Sessions:
+async function generateTodaysTrainingSessions() {
+    console.log('[⚙️] Adding new sessions...');
+
+    // 10:30 AM:
+    await sessionManager.saveSession(generateId(), {
+        type: 'Training',
+        date: generateTimestamp(10),
+        host: null,
+        trainers: [],
+        location: 'https://www.roblox.com/games/407106466/Munch-V1',
+        messageId: null,
+        channelId: null
+    });
+
+    // 2:30 PM:
+    await sessionManager.saveSession(generateId(), {
+        type: 'Training',
+        date: generateTimestamp(14),
+        host: null,
+        trainers: [],
+        location: 'https://www.roblox.com/games/407106466/Munch-V1',
+        messageId: null,
+        channelId: null
+    });
+
+    // 7:30 PM:
+    await sessionManager.saveSession(generateId(), {
+        type: 'Training',
+        date: generateTimestamp(19),
+        host: null,
+        trainers: [],
+        location: 'https://www.roblox.com/games/407106466/Munch-V1',
+        messageId: null,
+        channelId: null
+    });
+
+
+    // Debug:
+    console.log('[✅] Complete!');
+    const sessions = await sessionManager.readSessions();
+
+    const debugAllSessions = true
+    if (debugAllSessions) {
+        console.log('[ i ] All sessions:');
+        console.log(sessions);
+    }
+
+
+}
+
 
 // Generate Event Function:
 async function addSession() {
@@ -25,10 +79,7 @@ async function addSession() {
         channelId: null
     });
 
-    console.log('[✔︎] Complete!');
-    console.log('ALL SESSIONS:');
-    const sessions = await sessionManager.readSessions();
-    console.log(sessions);
+    
 }
 
 addSession().catch(console.error);
