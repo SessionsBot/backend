b0b9016af9b7833fa59d228d97de61df484eb285
br4dyb <halting.credo.00@icloud.com>
2025-05-13 15:13:19 -0500

    ~

--- commands/sessions/notifications.js
+++ commands/sessions/notifications.js
@@ -1,10 +1,9 @@
-const {
-    SlashCommandBuilder,
-    EmbedBuilder,
-    ActionRowBuilder,
-    ButtonBuilder,
-    ButtonStyle,
-    MessageFlags
+const { // Import Discord.js
+    EmbedBuilder, 
+    InteractionContextType, 
+    SlashCommandBuilder, 
+    MessageFlags,
+    SlashCommandBooleanOption,
 } = require('discord.js');
 
 const sessionManager = require('../../utils/sessions/sessionManager');
@@ -13,10 +12,19 @@ const data = new SlashCommandBuilder()
     .setName('notifications')
     .setDescription('Enable or disable upcoming event notifications.')
     .setContexts(InteractionContextType.Guild)
+    .addBooleanOption(option =>
+			option
+				.setName('Enabled')
+				.setDescription('TRUE = Enables Event Notifications, FALSE = Disables Event Notifications ')
+				.setRequired(true))
 
 async function execute(interaction) {
+
+    // Get selections:
+    let userChoice = interaction.options.getBoolean('Enabled');
+
     await interaction.reply({
-        content: 'Notification Command Used!',
+        content: 'Notification Command Used! | Selection: ' + '`' + userChoice + '`' ,
         flags: MessageFlags.Ephemeral
     });
 }
