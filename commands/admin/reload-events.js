4bdfe7e3dc07a95bae0dbe988ed671212e5cfa59
br4dyb <halting.credo.00@icloud.com>
2025-05-14 04:00:08 -0500

    ~

--- commands/admin/reload-events.js
+++ commands/admin/reload-events.js
@@ -23,12 +23,27 @@ async function execute(interaction) {
 
    
     try { // Respond / Refresh Events:
-        await interaction.reply('This command is currently unusable! :/')
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                .setColor(global.colors.warning)
+                .setTitle('⚠️ - Command Disabled!')
+                .setDescription('Unfortunately this command is not currently operational. . .')
+            ],
+            flags: MessageFlags.Ephemeral
+        })
 
     } catch (error) { // Error Occured:
         console.log('[!] An Error Occured - /reload-events');
         console.log(error);
     }
+
+    setTimeout(async () => {
+        await interaction.deleteReply().then().catch(error => {
+            console.log('Failed to delete reload events interaction reply:');
+            console.log(error)
+        })
+    }, 7000);
     
 }
 
