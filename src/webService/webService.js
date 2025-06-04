3918f4836a1a776a87a8067d993cdf35ed863a1b
br4dyb <halting.credo.00@icloud.com>
2025-06-04 16:49:14 -0500

    DISABILING CORS

--- src/webService/webService.js
+++ src/webService/webService.js
@@ -8,28 +8,28 @@ const app = express();
 app.use(express.static('webService'));
 
 // Security - Middleware:
-const cors = require('cors');
+// const cors = require('cors');
 
-const allowedOrigins = [
-  'https://brilliant-austina-sessions-bot-discord-5fa4fab2.koyeb.app',
-  'https://sessionsbot.fyi',
-  'http://localhost:5173' // for local dev
-];
+// const allowedOrigins = [
+//   'https://brilliant-austina-sessions-bot-discord-5fa4fab2.koyeb.app',
+//   'https://sessionsbot.fyi',
+//   'http://localhost:5173' // for local dev
+// ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl)
+//     if (!origin) return callback(null, true);
 
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'OPTIONS']
-}));
+//     if (allowedOrigins.includes(origin)) {
+//       return callback(null, true);
+//     } else {
+//       return callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'OPTIONS']
+// }));
 
 
 // Root/Status Requests:
