e8b8ae006296ef900983846a145c551f648cb09b
br4dyb <halting.credo.00@icloud.com>
2025-06-04 21:26:02 -0500

    ~

--- src/events/ready.js
+++ src/events/ready.js
@@ -38,32 +38,32 @@ module.exports = {
 
 
 
-		// [ GUILD MANAGER TESTING ] \\
-		const guildManager = require('../utils/guildManager.js')
-		let guildId = '1379160686629880028';
-		const scheduleId = 'shd_0218308213012';
-		const scheduleObject = {
-			sessionDateDaily: {
-				hour: 6,
-				minuets: 30,
-				timeZone: 'US Chicago'
-			},
-			roles: [
-				{ roleName: 'Role Name', roleCapcity: 1, users: [], roleDescription: 'This is an example role description.' },
-				{ roleName: 'Role2 Name', roleCapcity: 3, users: [], roleDescription: 'This is an example role description.' }
-			],
-			sessionTitle: 'Title Example',
-			sessionUrl: 'https://www.games.roblox.com'
-		}
+		// // [ GUILD MANAGER TESTING ] \\
+		// const guildManager = require('../utils/guildManager.js')
+		// let guildId = '1379160686629880028';
+		// const scheduleId = 'shd_0218308213012';
+		// const scheduleObject = {
+		// 	sessionDateDaily: {
+		// 		hour: 6,
+		// 		minuets: 30,
+		// 		timeZone: 'US Chicago'
+		// 	},
+		// 	roles: [
+		// 		{ roleName: 'Role Name', roleCapcity: 1, users: [], roleDescription: 'This is an example role description.' },
+		// 		{ roleName: 'Role2 Name', roleCapcity: 3, users: [], roleDescription: 'This is an example role description.' }
+		// 	],
+		// 	sessionTitle: 'Title Example',
+		// 	sessionUrl: 'https://www.games.roblox.com'
+		// }
 
-		// After Startup - Delay:
-		setTimeout(async () => {
-			console.log('[+] Attempting Firebase / Guild Manager Testing:')
+		// // After Startup - Delay:
+		// setTimeout(async () => {
+		// 	console.log('[+] Attempting Firebase / Guild Manager Testing:')
 
-			// const result = await guildManager.updateGuildDocField(guildId, 'upcomingSessions', {})
-			console.log('No testing done...')
+		// 	// const result = await guildManager.updateGuildDocField(guildId, 'upcomingSessions', {})
+		// 	console.log('No testing done...')
 
-		}, 2_500);
+		// }, 2_500);
 
 
 	},
