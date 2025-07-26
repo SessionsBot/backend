8e4188a02dbbb4f5254b76b264d68b11ee691ae3
br4dyb <halting.credo.00@icloud.com>
2025-07-26 14:28:25 -0500

    ~

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,5 +1,5 @@
 module.exports = {
-    botVersion: '1.1.8b',
+    botVersion: '1.1.8c',
     frontend_Url: 'https://sessionsbot.fyi',
     outputDebug_General: true,
     outputDebug_InDepth: false,
