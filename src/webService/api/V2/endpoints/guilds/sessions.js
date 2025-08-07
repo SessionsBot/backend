0173bad3680e95324ba42e7ead0c942ee1751664
br4dyb <halting.credo.00@icloud.com>
2025-08-06 22:28:26 -0500

    feat: Sessions Endpoints

--- src/webService/api/V2/endpoints/guilds/sessions.js
+++ src/webService/api/V2/endpoints/guilds/sessions.js
@@ -1,25 +1,71 @@
 //------------------------------------------[ Imports ]------------------------------------------\\
 const express = require('express');
 const router = express.Router({mergeParams: true})
-const responder = require('../../utils/responder')
+const responder = require('../../utils/responder');
+const { HttpStatusCode } = require('axios');
+const guildManager = require('../../../../../utils/guildManager');
 
 
 
 //-----------------------------------------[ Endpoints ]-----------------------------------------\\
 // GET/FETCH Session:
-router.get('/:sessionId', async (req, res) => {
-    const fetchId = req.params.sessionId
+router.get('/:sessionId', async (req, res) => { try {
+    // 1. Get parameters:
+    const sessionId = req.params?.sessionId
     const guildId = req.params?.guildId
-    if(!fetchId) return responder.errored(res, `invalid "sessionId" provided`)
-    else return responder.succeeded(res, `[GET] Request send for session ${fetchId} | ${guildId}`)
-})
+    // 2. Confirm parameters:
+    if(!sessionId) throw {message: `Invalid Input - Missing 'sessionId'`, code: 400};
+    if(!guildId) throw {message: `Invalid Input - Missing 'guildId'`, code: 400};
+    // 3. Get/Read Session:
+    const readAllResults = await guildManager.guildSessions(guildId).getSessions()
+    if(!readAllResults.success) throw {message: `Internal Error - Failed to fetch guild sessions for guild(${guildId}).`, code: 500};
+    const allSessions = readAllResults.data;
+    const reqSession = allSessions[sessionId];
+    if(!reqSession) throw {message: `Not Found - Cannot find session(${sessionId}) in guild(${guildId}).`, code: 404};
+    
+    // 4. Return results:
+    return responder.succeeded(res, reqSession)
+    
+} catch (err) {
+
+    if(err?.message){ // Known Error:
+        return responder.errored(res, err.message, err?.code || 400)
+    }else{ // Unknown Error:
+        return responder.errored(res, 'An internal API error has occurred, please try again.', 500)
+    }
+    
+}})
 
 // PATCH/UPDATE Session:
-router.patch('/:sessionId', async (req, res) => {
-    const fetchId = req.params.sessionId
-    if(!fetchId) return responder.errored(res, `invalid "sessionId" provided`)
-    else return responder.succeeded(res, `[PATCH] Request send for session ${fetchId}`)
-})
+router.patch('/:sessionId', async (req, res) => { try {
+    // 1. Get parameters:
+    const sessionId = req.params?.sessionId
+    const guildId = req.params?.guildId
+    const newSessionData = req.body?.sessionData
+    // 2. Confirm parameters:
+    if(!sessionId) throw {message: `Invalid Input - Missing 'sessionId'`, code: 400};
+    if(!newSessionData) throw {message: `Invalid Input - Missing 'sessionData' inside body of request.`, code: 400};
+    if(!guildId) throw {message: `Invalid Input - Missing 'guildId'`, code: 400};
+    
+    // 3. Get/Read Session:
+    const readAllResults = await guildManager.guildSessions(guildId).getSessions()
+    if(!readAllResults.success) throw {message: `Internal Error - Failed to fetch guild sessions for guild(${guildId}).`, code: 500};
+    const allSessions = readAllResults.data;
+    const reqSession = allSessions[sessionId];
+    if(!reqSession) throw {message: `Not Found - Cannot find session(${sessionId}) in guild(${guildId}).`, code: 404};
+    
+    // 4. Return results:
+    return responder.succeeded(res, {oldData: reqSession, newData: newSessionData})
+    
+} catch (err) {
+
+    if(err?.message){ // Known Error:
+        return responder.errored(res, err?.message, err?.code || 400)
+    }else{ // Unknown Error:
+        return responder.errored(res, 'An internal API error has occurred, please try again.', 500)
+    }
+    
+}})
 
 //-------------------------------------[ Export Endpoints ]-------------------------------------\\
 module.exports = router
