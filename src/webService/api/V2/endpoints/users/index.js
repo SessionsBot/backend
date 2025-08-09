c55487edfdbdb8a77687edaae6a63ae3c9f84367
br4dyb <halting.credo.00@icloud.com>
2025-08-09 16:22:38 -0500

    fix: safely fetch discord API within user endpoints

--- src/webService/api/V2/endpoints/users/index.js
+++ src/webService/api/V2/endpoints/users/index.js
@@ -145,16 +145,20 @@ router.get('/:userId', async (req, res) => {
     if(!fetchId) return responder.errored(res, `Invalid "userId" provided!`);
 
     // Get discord user data:
-    const discordUserReq = await axios.get(`https://discord.com/api/v10/users/${fetchId}`, {
-        headers: {
-            Authorization: `Bot ${BOT_TOKEN}`
-        }
-    });
-    let discordUserData = discordUserReq?.data;
-    if(!discordUserData) return responder.errored(res, `Failed to fetch Discord user data!`);
-
-    // Return data:
-    return responder.succeeded(res, discordUserData);
+    try {
+        const discordUserReq = await axios.get(`https://discord.com/api/v10/users/${fetchId}`, {
+            headers: {
+                Authorization: `Bot ${BOT_TOKEN}`
+            }
+        });
+        let discordUserData = discordUserReq?.data;
+        if(!discordUserData) return responder.errored(res, `Failed to fetch Discord user data!`);
+    
+        // Return data:
+        return responder.succeeded(res, discordUserData);
+    } catch (err) {
+        return responder.errored(res, 'Failed to fetch Discord user data from Discord API.', 500);
+    }
 
 
 })
@@ -168,10 +172,10 @@ router.delete('/:userId', verifyToken, async (req, res) => {
     if(req.user.id != deleteId) return responder.errored(res, `Invalid Permissions - You can only delete your own account!`, 403)
 
     // Delete user from Firebase Auth:
-    await auth.deleteUser(deleteId).catch((e) => {return responder.errored(res, e?.message | 'Unknown Firebase deletion error!', 500) })
+    await auth.deleteUser(deleteId).catch((e) => {return responder.errored(res, e?.message | 'Unknown Error - Internal - Firebase deletion error', 500) })
 
     // Return Success:
-    return responder.succeeded(res, `[DELETE] User ${deleteId} has been successfully deleted!`)
+    return responder.succeeded(res, `User ${deleteId} has been successfully deleted!`)
 
 })
 
