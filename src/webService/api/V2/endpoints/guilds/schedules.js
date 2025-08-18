b6648ab747cda5199804b3a3a6b4ae8e39a41f4c
br4dyb <halting.credo.00@icloud.com>
2025-08-18 02:21:54 -0500

    feat(api): modify schedule by id

--- src/webService/api/V2/endpoints/guilds/schedules.js
+++ src/webService/api/V2/endpoints/guilds/schedules.js
@@ -50,7 +50,32 @@ router.post('/', verifyToken, verifyGuildMember, async (req, res) => { try {
     if(!scheduleData) throw {message: `Invalid Input - Missing 'scheduleData' from request body`, code: 400};
     // 3. Add Schedule Attempt:
     const addSchAttempt = await guildManager.guildSchedules(guildId).addSessionSchedule(scheduleData)
-    if(!addSchAttempt.success) throw {message: `Internal Error - Failed to add schedule to guild data, please try again!`, code: 400};
+    if(!addSchAttempt.success) throw {message: `Internal Error - Failed to add schedule to guild data, please try again!`, code: 500};
+    // 4. Return Result:
+    return responder.succeeded(res, ['Schedule Added!', {guildId, scheduleData}]);
+
+} catch (err) {
+    if(err?.message) { // Known Error:
+        return responder.errored(res, err.message, err?.code || 400)
+    } else { // Unknown Error:
+        console.warn('{!}[API] Failed to add schedule to guild:', JSON.stringify(err, null, 2))
+        return responder.errored(res, 'An internal API error has occurred, please try again.', 500)
+    }
+}})
+
+// PATCH/UPDATE - Update Schedule:
+router.patch('/:scheduleId', verifyToken, verifyGuildMember, async (req, res) => { try {
+    // 1. Get parameters:
+    const scheduleId = req.params?.scheduleId
+    const guildId = req.params?.guildId
+    const scheduleData = req.body?.scheduleData
+    // 2. Confirm parameters:
+    if(!scheduleId) throw {message: `Invalid Input - Missing 'scheduleId'`, code: 400};
+    if(!guildId) throw {message: `Invalid Input - Missing 'guildId'`, code: 400};
+    if(!scheduleData) throw {message: `Invalid Input - Missing 'scheduleData' from request body`, code: 400};
+    // 3. Modify Schedule Attempt:
+    const modifySchAttempt = await guildManager.guildSchedules(guildId).modifySessionSchedule(scheduleId, scheduleData)
+    if(!modifySchAttempt.success) throw {message: `Internal Error - Failed to add schedule to guild data, please try again!`, code: 500};
     // 4. Return Result:
     return responder.succeeded(res, ['Schedule Added!', {guildId, scheduleData}]);
 
