a908a5c8b42f4754e68e82379a4c081f1912a494
br4dyb <halting.credo.00@icloud.com>
2025-08-25 13:33:29 -0500

    fix: bot invite/welcome msg

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,5 +1,5 @@
 // Imports:
-import {  Events, ChannelType, PermissionsBitField  } from "discord.js";
+import {  Events, ChannelType, ButtonBuilder, PermissionsBitField, ContainerBuilder, SeparatorBuilder, SectionBuilder, TextDisplayBuilder, ButtonStyle, MessageFlags, ThumbnailBuilder, ActionRowBuilder  } from "discord.js";
 import global from "../utils/global.js";
 import guildManager from "../utils/guildManager.js";
 import logtail from "../utils/logtail.js";
@@ -7,6 +7,7 @@ import logtail from "../utils/logtail.js";
 // Event:
 export default {
     name: Events.GuildCreate,
+    /** @param {import('discord.js').Guild} guild */
     async execute(guild) {
 
         // Debug New Guild:
@@ -24,24 +25,56 @@ export default {
         }
 
         
-        // Send Welcome/Setup Message:
-        const welcomeMessage = `👋 Hi! I'm your new bot. Please visit ${global.frontend_Url}/api/guild-setup?guildId=${guild.id} to get started.`;
+        // 2. Build Welcome/Setup Message:
+        const welcomeMessage = new ContainerBuilder() // `👋 Hi! I'm your new bot. Please visit ${global.frontend_Url}/api/guild-setup?guildId=${guild.id} to get started.`;
+        // const separator = new SeparatorBuilder()
+        welcomeMessage.setAccentColor(0xb28ef6)
 
-        // 2. Attempt to send in default system channel:
+        const setupButton = new ButtonBuilder()
+            .setLabel('Setup Bot')
+            .setEmoji('🥳')
+            .setStyle(ButtonStyle.Link)
+            .setURL(`${global.frontend_Url}/api/guild-setup?guildId=${guild.id}`);
+
+        const helpButton = new ButtonBuilder()
+            .setLabel('Need Help?')
+            .setEmoji('🤔')
+            .setStyle(ButtonStyle.Link)
+            .setURL(`${global.frontend_Url}/support`);
+
+        
+
+        const section = new SectionBuilder()
+            .addTextDisplayComponents(new TextDisplayBuilder()
+                .setContent(`## Welcome to Sessions Bot! \n *Please click below to setup your server...* \n @here`))
+            .setThumbnailAccessory(new ThumbnailBuilder()
+                .setURL(`https://avatars.githubusercontent.com/u/220646615?s=200&v=4`)
+                .setDescription('Sessions Bot Logo'))
+        welcomeMessage.addSectionComponents(section);
+
+        welcomeMessage.addActionRowComponents(new ActionRowBuilder().setComponents([setupButton, helpButton]));
+
+        
+
+
+        // 3. Attempt to send in default system channel:
         if (
             guild.systemChannel &&
             guild.systemChannel.viewable &&
             guild.systemChannel.permissionsFor(guild.members.me).has(PermissionsBitField.Flags.SendMessages)
         ){
             try {
-                await guild.systemChannel.send(welcomeMessage);
+                await guild.systemChannel.send({
+                    components: [welcomeMessage],
+                    flags: MessageFlags.IsComponentsV2
+                });
                 return;
             } catch (err) {
                 console.warn('Failed to send to system channel:', err);
             }
         }
 
-        // 3. Attempt to send in any chat-able channel:
+        // 4. Attempt to send in any chat-able channel:
         const fallbackChannel = guild.channels.cache.find(channel =>
             channel.type === ChannelType.GuildText &&
             channel.viewable &&
@@ -50,17 +83,23 @@ export default {
 
         if (fallbackChannel) {
             try {
-                await fallbackChannel.send(welcomeMessage);
+                await fallbackChannel.send({
+                    components: [welcomeMessage],
+                    flags: MessageFlags.IsComponentsV2
+                });
                 return;
             } catch (err) {
                 console.warn(`Failed to send to fallback channel (${fallbackChannel.name}):`, err);
             }
         }
 
-        // 4. Direct message server owner:
+        // 5. Direct message server owner:
         try {
             const owner = await guild.fetchOwner();
-            await owner.send(`Thanks for adding me to **${guild.name}**!\n\n${welcomeMessage}`);
+            await owner.send({
+                    components: [welcomeMessage],
+                    flags: MessageFlags.IsComponentsV2
+                });
             return;
         } catch (err) {
             console.warn('Failed to DM the guild owner:', err);
