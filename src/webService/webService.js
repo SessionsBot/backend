b42765197525547635204ec831eb98449770b4d8
br4dyb <halting.credo.00@icloud.com>
2025-08-26 00:01:41 -0500

    refactor: debug cors declines

--- src/webService/webService.js
+++ src/webService/webService.js
@@ -20,7 +20,7 @@ app.use(cors({
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
     } else {
-      return callback(new Error('Not allowed by CORS'));
+      return callback(new Error(`Not allowed by CORS - origin: ${origin}`));
     }
   },
   credentials: true,
