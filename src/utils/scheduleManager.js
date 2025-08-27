5130b1e0f2ca020c16badaa1d533a66b3ffd7cb3
br4dyb <halting.credo.00@icloud.com>
2025-08-27 15:43:11 -0500

    test: remove test flag

--- src/utils/scheduleManager.js
+++ src/utils/scheduleManager.js
@@ -6,7 +6,7 @@ import { db } from "./firebase.js";
 import axios from "axios"; // Import Firebase
 // Dev Testing
 const devTesting = {
-    enabled: true,
+    enabled: false,
     guildId: "1379160686629880028",
 };
 if (devTesting.enabled)
