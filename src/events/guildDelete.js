5c3a88398a04fc2191b0eaacc1900122f0cd89e2
br4dyb <halting.credo.00@icloud.com>
2025-10-09 22:39:41 -0500

    fix: clear post sch for removing guilds

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -4,6 +4,7 @@ import global from "../utils/global.js";
 import guildManager from "../utils/guildManager.js";
 import logtail from "../utils/logs/logtail.js";
 import {  DateTime  } from "luxon";
+import scheduleManager from "../utils/scheduleManager.js";
 
 
 
@@ -13,8 +14,11 @@ export default {
     name: Events.GuildDelete,
     /** @param {import('discord.js').Guild} guild */
     async execute(guild) {
+        // Destroy Session Schedule - If Exists:
+        scheduleManager.cancelGuildSessionsPost(guild.id);
+
         // Debug - Leaving Guild:
-        const joinedAtDate = DateTime.fromMillis(guild?.joinedTimestamp).setZone('America/Chicago').toLocaleString(DateTime.DATETIME_FULL)
+        const joinedAtDate = DateTime.fromMillis(guild?.joinedTimestamp).setZone('America/Chicago').toLocaleString(DateTime.DATETIME_FULL);
         logtail.info(`[-] Guild ${guild?.name} (${guild?.id}) has removed Sessions Bot!`, {guildName: guild?.name, memberCount: guild?.memberCount, joinedAt: joinedAtDate});
 
         // Move guild to archive within database:
